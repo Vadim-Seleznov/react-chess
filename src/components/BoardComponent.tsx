6b0cb9494d1819a6e3a0015517db513cdcbb9a6f
Vadim Seleznov <vadimseleznov929@gmail.com>
2026-02-11 12:10:03 -0500

    basic selection

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,7 +1,8 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import "../App.css";
 import { Board } from "../models/Board";
 import CellComponent from "./CellComponent";
+import { Cell } from "../models/Cell";
 
 interface BoardProps {
   board: Board;
@@ -9,12 +10,29 @@ interface BoardProps {
 }
 
 const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+  const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
+
+  function click(cell: Cell) {
+    if (cell.figure) {
+      setSelectedCell(cell);
+    }
+  }
+
   return (
     <div className="board">
       {board.cells.map((row, index) => (
         <React.Fragment key={index}>
           {row.map((cell) => (
-            <CellComponent cell={cell} key={cell.id} />
+            <CellComponent
+              cell={cell}
+              click={click}
+              key={cell.id}
+              selected={
+                cell.x === selectedCell?.x && cell.y === selectedCell?.y
+                  ? true
+                  : false
+              }
+            />
           ))}
         </React.Fragment>
       ))}
